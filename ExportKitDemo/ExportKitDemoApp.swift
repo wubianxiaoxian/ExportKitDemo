b18bb02a4275d9c363ba4b36adf978df9a528268
KentSun <kaifeng@base64.amsterdam>
2025-09-30 15:40:28 +0800

    Initial Commit

---
+++ ExportKitDemo/ExportKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExportKitDemoApp.swift
+//  ExportKitDemo
+//
+//  Created by kent.sun on 2025/9/30.
+//
+
+import SwiftUI
+
+@main
+struct ExportKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExportKitDemoTests/ExportKitDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExportKitDemoTests.swift
+//  ExportKitDemoTests
+//
+//  Created by kent.sun on 2025/9/30.
+//
+
+import Testing
+@testable import ExportKitDemo
+
+struct ExportKitDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
